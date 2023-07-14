7e2c46680cac989f86fb277e91df6e833b4c01a2
Your Name <nithin4517@gmail.com>
2023-07-14 10:10:05 -0400

    Add sorting feature to table

--- src/components/Dashboard/Table.js
+++ src/components/Dashboard/Table.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaSortUp, FaSortDown } from 'react-icons/fa';
 
 const Table = ({ employees, handleEdit, handleDelete }) => {
+  const [sortColumn, setSortColumn] = useState('');
+  const [sortOrder, setSortOrder] = useState('');
+
   employees.forEach((employee, i) => {
     employee.id = i + 1;
   });
@@ -11,45 +15,74 @@ const Table = ({ employees, handleEdit, handleDelete }) => {
     minimumFractionDigits: null,
   });
 
+  const toggleSort = (column) => {
+    if (column === sortColumn) {
+      setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
+    } else {
+      setSortColumn(column);
+      setSortOrder('asc');
+    }
+  };
+
+  const getSortIcon = (column) => {
+    if (column === sortColumn) {
+      return sortOrder === 'asc' ? <FaSortUp /> : <FaSortDown />;
+    }
+    return null;
+  };
+
+  const sortedEmployees = [...employees].sort((a, b) => {
+    if (sortColumn) {
+      const aValue = a[sortColumn].toUpperCase();
+      const bValue = b[sortColumn].toUpperCase();
+      return sortOrder === 'asc' ? aValue.localeCompare(bValue) : bValue.localeCompare(aValue);
+    }
+    return 0;
+  });
+
   return (
     <div className="contain-table">
       <table className="striped-table">
         <thead>
           <tr>
             <th>No.</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Email</th>
-            <th>Salary</th>
-            <th>Date</th>
+            <th onClick={() => toggleSort('firstName')}>
+              First Name {getSortIcon('firstName')}
+            </th>
+            <th onClick={() => toggleSort('lastName')}>
+              Last Name {getSortIcon('lastName')}
+            </th>
+            <th onClick={() => toggleSort('email')}>
+              Email {getSortIcon('email')}
+            </th>
+            <th onClick={() => toggleSort('salary')}>
+              Salary {getSortIcon('salary')}
+            </th>
+            <th onClick={() => toggleSort('date')}>
+              Date {getSortIcon('date')}
+            </th>
             <th colSpan={2} className="text-center">
               Actions
             </th>
           </tr>
         </thead>
         <tbody>
-          {employees.length > 0 ? (
-            employees.map((employee, i) => (
+          {sortedEmployees.length > 0 ? (
+            sortedEmployees.map((employee, i) => (
               <tr key={employee.id}>
                 <td>{i + 1}</td>
                 <td>{employee.firstName}</td>
                 <td>{employee.lastName}</td>
                 <td>{employee.email}</td>
                 <td>{formatter.format(employee.salary)}</td>
-                <td>{employee.date} </td>
+                <td>{employee.date}</td>
                 <td className="text-right">
-                  <button
-                    onClick={() => handleEdit(employee.id)}
-                    className="button muted-button"
-                  >
+                  <button onClick={() => handleEdit(employee.id)} className="button muted-button">
                     Edit
                   </button>
                 </td>
                 <td className="text-left">
-                  <button
-                    onClick={() => handleDelete(employee.id)}
-                    className="button muted-button"
-                  >
+                  <button onClick={() => handleDelete(employee.id)} className="button muted-button">
                     Delete
                   </button>
                 </td>
